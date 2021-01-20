adc4e195c8f55029ec485479964da2d18778a1d4
Alexander10531 <AlexanderTejedaBarahona10@gmail.com>
2021-01-20 02:08:59 -0600

    Creacion de instagram

---
+++ 27/main.js
@@ -0,0 +1,9 @@
+//  1. Carga la paginas a la pagina que se le pasa como argumento.
+//  document.location.assign("../26/main.html");
+//  2. Recarga la pagina con las opciones de manejar el cacheado 
+//  document.location.reload()
+//  2.1 Recarga sin usar el cacheado 
+//  document.locations.reload(True);
+//  3. Recarga la pagina sustituyendo la misma en el historial
+//  documentation.location.replace(direccion);
+// 
